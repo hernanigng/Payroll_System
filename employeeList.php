e25f3818d80c537574f428ad569bf6441bd78a85
beaaa21 <azucena.beatriz9@gmail.com>
2024-03-25 22:15:01 +0800

    Create employeeList.php

---
+++ employeeList.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> iREPLY | Employee List </title>
+    </head>
+
+        <body> 
+
+            <table id="table_id" class="display">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>James</td>
+                        <td>24</td>
+                    </tr>
+                    <tr>
+                        <td>Raj</td>
+                        <td>25</td>
+                    </tr>
+                </tbody>
+            </table>
+        </body>
+</html>
