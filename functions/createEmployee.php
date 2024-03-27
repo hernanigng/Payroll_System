2f0a5d7e5fd704ea451d9a58c98172290657213d
beaaa21 <azucena.beatriz9@gmail.com>
2024-03-28 04:46:11 +0800

    all insert working

--- functions/createEmployee.php
+++ functions/createEmployee.php
@@ -1,4 +1,5 @@
 <?php
+print_r($_POST); 
 
 $conn = mysqli_connect("localhost", "root", "", "ireply_payroll_db");
 
@@ -17,23 +18,37 @@ $startDate = $_POST['createStartDate'];
 $monthlySalary = $_POST['createMonthlySalary'];
 $accntBonus = $_POST['createAccountBonus'];
 $client = $_POST['createClient'];
-$position = $_POST['createPostion'];
+$position = $_POST['createPosition']; 
 $employmentStatus = $_POST['createEmploymentStatus'];
 
+$sss = $_POST['createSSS'];
+$pagibig = $_POST['createPagibig'];
+$philhealth = $_POST['createPhilhealth'];
+$tin = $_POST['createTin'];
+$sssContrib = $_POST['createSSSContrib'];
+$pagibigContrib = $_POST['createPagibigContrib'];
+$philhealthContrib = $_POST['createPhilhealthContrib'];
+$taxPercent = $_POST['createTaxPercent'];
+
 $conn->query("INSERT INTO tbl_employee (firstname, middlename, lastname, address, birthdate, contact_num, civilstatus, personal_email, work_email, employee_type,
-start_date, monthly_salary, account_bonus, client, position, employment_status)
+start_date, monthly_salary, account_bonus, client, position, employment_status, sss_num, pagibig_num, philhealth_num, tin_num, sss_con, pagibig_con, philhealth_con, tax_percentage)
 VALUES ('$firstName', '$midName', '$lastName', '$address', '$birthdate', '$contactNum', '$civilStat', '$persEmail', '$workEmail', '$employeeType', '$startDate',
-'monthlySalary', '$accntBonus', '$client', '$position', '$employmentStatus')");
+'$monthlySalary', '$accntBonus', '$client', '$position', '$employmentStatus', '$sss', '$pagibig', '$philhealth', '$tin', '$sssContrib', '$pagibigContrib', '$philhealthContrib', '$taxPercent')");
 
-$last_id = $conn->insert_id;
+if (!$conn) {
+  $error = mysqli_error($conn);
+  echo json_encode(array('error' => $error));
+} else {
+  $last_id = $conn->insert_id;
 
-echo json_encode( 
-  array(
-    'last_id' => $last_id,
-    'firstname' => $firstName,
-    'lastname' => $lastName,
-    'message' => '<span class="alert alert-info">Test Message</span>'
-));
+  $response = array(
+      'last_id' => $last_id,
+      'firstname' => $firstName,
+      'lastname' => $lastName,
+      'message' => '<span class="alert alert-info">Test Message</span>'
+  );
 
+  echo json_encode($response);
+}
 $conn->close();
 ?>
